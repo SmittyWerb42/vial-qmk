6b3752cbdffbc67872f90064d0f58f12e65de584
csmelser <65476155+csmelser@users.noreply.github.com>
2024-05-26 13:57:04 -0500

    Added other vial files

---
+++ keyboards/kaly/kaly42/keymaps/SmittyWerb42/config.h
@@ -0,0 +1,10 @@
+
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0x3D, 0x8F, 0xB6, 0x3F, 0xB8, 0x2B, 0x5D, 0xA6}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+
