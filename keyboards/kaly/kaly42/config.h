5ca28d483b9966a7ac2825c53591ee4106eef64d
Caleb <calebsmelser@gmail.com>
2025-06-26 09:35:23 -0500

    Update config.h

--- keyboards/kaly/kaly42/config.h
+++ keyboards/kaly/kaly42/config.h
@@ -12,13 +12,11 @@
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 
 // Auto Shift
-#define NO_AUTO_SHIFT_ALPHA
 #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
 #define AUTO_SHIFT_NO_SETUP
